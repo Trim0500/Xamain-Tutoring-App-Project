3c1b745cfbff8db4cfa8f0599ae0c048aa38c4f6
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2021-11-23 10:45:14 -0500

    Implemented the splash screen

---
+++ TutoringAppProject.Android/SplashAct.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace TutoringAppProject.Droid
+{
+    [Activity(Label = "TutoringAppProject", Icon = "@mipmap/icon", NoHistory = true, Theme = "@style/SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    public class SplashAct : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            Thread.Sleep(2000);
+            Finish();
+        }
+        public override void OnBackPressed()
+        {
+            //base.OnBackPressed();
+        }
+    }
+}
