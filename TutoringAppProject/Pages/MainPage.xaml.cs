bda139e29a028ea991a49ff9dc76faffcac76998
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2021-11-26 09:08:50 -0500

    Implemenmted the Verification

---
+++ TutoringAppProject/Pages/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TutoringAppProject.Pages;
+using Xamarin.Forms;
+
+namespace TutoringAppProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void teacher_toolbar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tutor_toolbar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void student_toolbar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void logout_toolbar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+
+        private async void user_toolbar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new UserList());
+        }
+    }
+}
