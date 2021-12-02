be2ad6d60ece0b33e38467fdcc55efd284bb13c0
Tristan Lafleur <tristanblacklafleur@hotmail.ca>
2021-12-02 09:55:45 -0500

    Created the TeacherHome page and allow for navigation to a custom tutor and student list

--- TutoringAppProject/Pages/TeacherOperations/TeacherHome.xaml.cs
+++ TutoringAppProject/Pages/TeacherOperations/TeacherHome.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TutoringAppProject.Pages.TeacherOperations.StudentPages;
+using TutoringAppProject.Pages.TeacherOperations.TutorPages;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +16,27 @@ namespace TutoringAppProject.Pages.TeacherOperations
         public TeacherHome()
         {
             InitializeComponent();
+        }
+
+        private async void TutorList_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TutorHours());
+        }
+
+        private async void StudentList_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new StudentVisits());
+        }
+
+        private async void LogoutToolbar_Clicked(object sender, EventArgs e)
+        {
+            // display login out confirmation
+            var answer = await DisplayAlert("Logout", "Are you sure you want to logout?", "Yes", "No");
+            if (answer)
+            {
+                // logout
+                await Navigation.PopAsync();
+            }
         }
     }
 }
